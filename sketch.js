f8798d8bde0f04b0e5dc66826e115775eae067d3
superaarav <70596928+superaarav@users.noreply.github.com>
2020-09-02 13:00:51 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var fixedRect;
+var movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = 'green';
+
+  movingRect = createSprite(400,800,80,30);
+  movingRect.shapeColor = "green";
+
+  movingRect.velocityY  = -5;
+  fixedRect.velocityY = 5;
+}
+
+function draw() {
+  background(0);  
+  
+ /* movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x-fixedRect.x < fixedRect.width/2 + movingRect.width/2 
+    && fixedRect.x - movingRect.x<fixedRect.width/2 + movingRect.width/2 &&
+    movingRect.y-fixedRect.y < fixedRect.height/2 + movingRect.height/2 
+    && fixedRect.y - movingRect.y<fixedRect.height/2 + movingRect.height/2) {
+    movingRect.shapeColor = 'red';
+    fixedRect.shapeColor = 'red';
+  }
+
+  else {
+    movingRect.shapeColor = 'green';
+    fixedRect.shapeColor = 'green';
+  }*/
+  if(movingRect.y-fixedRect.y < fixedRect.height/2 + movingRect.height/2 
+    && fixedRect.y - movingRect.y<fixedRect.height/2 + movingRect.height/2) {
+      movingRect.velocityY = movingRect.velocityY*(-1);
+      fixedRect.velocityY = fixedRect.velocityY*(-1);
+
+    }
+  
+
+  drawSprites();
+}
